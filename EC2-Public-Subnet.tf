dcce499806c40768e4b66d721c4007d4e2dab9cf
UGUP <upma121gupta@gmail.com>
2022-12-25 16:31:25 +0530

    SMll change

--- EC2-Public-Subnet.tf
+++ EC2-Public-Subnet.tf
@@ -1,7 +1,7 @@
 #Creating EC2 instance and SSH into it
 
 
-resource "aws_instance" "Terraform2"{
+resource "aws_instance" "Terraform"{
      ami = "ami-0a6b2839d44d781b2" # us-east-1 
      instance_type = "t2.medium" 
      subnet_id = aws_subnet.public-subnet1.id 
