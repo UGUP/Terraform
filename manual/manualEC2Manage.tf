c5007aaf81fbc7661d6a1e4fb2b88c851c8e1c4d
UGUP <upma121gupta@gmail.com>
2022-12-26 14:05:07 +0530

    Added manual managemt of EC2

--- manual/manualEC2Manage.tf
+++ manual/manualEC2Manage.tf
@@ -1,7 +1,7 @@
 #Managing EC2 instance using Terraform 
 
 
-resource "aws_instance" "Terraform"{
+resource "aws_instance" "Manual"{
      ami = var.ami # us-east-1 
      instance_type = var.instancetype
      subnet_id = "subnet-4d33f32b" 
