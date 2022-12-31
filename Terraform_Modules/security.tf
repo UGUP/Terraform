6f264fcfbb8c316eee93970d82d85db38527849f
UGUP <upma121gupta@gmail.com>
2022-12-31 15:31:53 +0530

    Added module -VPC

--- Terraform_Modules/security.tf
+++ Terraform_Modules/security.tf
@@ -1,7 +1,7 @@
 module "vote_service_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "sg"
+  name        = "my-security-group"
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = module.vpc.vpc_id
 
