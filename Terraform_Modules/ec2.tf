c9f2f902fe8aace1ed27f2aca481af222666d46d
UGUP <upma121gupta@gmail.com>
2022-12-31 16:12:48 +0530

    ignore

--- Terraform_Modules/ec2.tf
+++ Terraform_Modules/ec2.tf
@@ -1,18 +1,18 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+# module "ec2_instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   version = "~> 3.0"
 
-  name = "single-instance"
+#   name = "single-instance"
 
-  ami                    = "ami-0a6b2839d44d781b2"
-  instance_type          = "t2.micro"
-  key_name               = "jenk4"
-  monitoring             = true
-  vpc_security_group_ids = [module.vote_service_sg.security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
+#   ami                    = "ami-0a6b2839d44d781b2"
+#   instance_type          = "t2.micro"
+#   key_name               = "jenk4"
+#   monitoring             = true
+#   vpc_security_group_ids = [module.vote_service_sg.security_group_id]
+#   subnet_id              = module.vpc.public_subnets[0]
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
--- Terraform_Modules/security.tf
+++ Terraform_Modules/security.tf
@@ -1,21 +1,21 @@
-module "vote_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+# module "vote_service_sg" {
+#   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "my-security-group"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = module.vpc.vpc_id
+#   name        = "my-security-group"
+#   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "0.0.0.0/16"
-    },
+#   ingress_cidr_blocks      = ["10.10.0.0/16"]
+#   ingress_rules            = ["https-443-tcp"]
+#   ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 22
+#       to_port     = 22
+#       protocol    = "tcp"
+#       description = "User-service ports"
+#       cidr_blocks = "0.0.0.0/16"
+#     },
   
  
-  ]
-}
+#   ]
+# }
